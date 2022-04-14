001a718194d28c8b4f45fb6f7bd8a911e0e69895
Gerard <gerardalvariz@gmail.com>
2022-04-14 02:30:26 +0200

    Create usuarios, sign in, mensajes de error en submit y sign in y reconducidas las rutas de submit al crear

--- controller/userController.js
+++ controller/userController.js
@@ -1,9 +1,28 @@
-const user = require("../models/users");
+const { render } = require("../app");
+const User = require("../models/user");
 
 const mostrarForm = async (req, res) => {
-  res.render("login", {});
+  res.render('login');
 };
+
+
+const createUser =  async (req,res) => {
+  const { username,  password } = req.body;
+     User.findOne({ username: username }).then((user) => {
+      if (user) {
+        console.log("username exists");
+        res.render('login', { errorMessage: 'El nombre de usuario escogido ya existe' });
+      } else {
+        var newUser = new User({username,password})
+        newUser.save() 
+        res.redirect('/')
+      }
+      
+    }); 
+  };
+
 
 module.exports = {
   mostrarForm,
+  createUser
 };
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 
 const submissionController = require("../controller/submissionController");
-const userController = require("../controller/userController");
 
 router.get("/", submissionController.mostrarIndex);
 
@@ -10,10 +9,10 @@ router.get("/newest", submissionController.mostrarNewest);
 
 router.get("/submit", submissionController.mostrarSubmissionForm);
 
-router.post("/create", submissionController.createSubmisson);
+router.post("/submit", submissionController.createSubmisson);
 
 router.get("/submission/:id", submissionController.mostrarSubmission);
 
-router.get("/login", userController.mostrarForm);
+
 
 module.exports = router;
