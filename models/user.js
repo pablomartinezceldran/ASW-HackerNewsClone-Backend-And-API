2aa2dffb8fd4e7b53ec21ebbd6df8ad6dc9bd574
Gerard <77858157+gerardalvarez@users.noreply.github.com>
2022-04-20 19:03:51 +0200

    views bugs fixed

--- models/user.js
+++ models/user.js
@@ -6,6 +6,10 @@ var userSchema = new mongoose.Schema(
         type: String,
             required: true,
     },
+    username: {
+      type: String,
+      required: true,
+    },
     password: {
       type: String,
       required: true,
