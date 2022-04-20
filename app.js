7834846770f9f5117e28d17bc9bb9a8f00ae06ce
Marc Castells <marc.casellsd@gmail.com>
2022-04-20 21:51:15 +0200

    Heroku creation

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ dbConnect();
 module.exports = app;
 
 //Puertos
-const port = 3000;
-app.listen(port, () => {
-  console.log("Listening on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
